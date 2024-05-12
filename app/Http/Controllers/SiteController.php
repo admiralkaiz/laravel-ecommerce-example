3ebc6c0450f7b6e7e9ab407919d3b50d7bfe06f9
Großadmiral Kaiz ☪️⚓ (Admie) <91956750+admiralkaiz@users.noreply.github.com>
2024-05-12 14:16:14 +0700

    Update SiteController.php

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -9,25 +9,25 @@ use Illuminate\Support\Facades\Auth;
 class SiteController extends Controller
 {
     public function auth(Request $req) {
-        $u = User::where([
-            ['email', $req->em],
-            ['password', $req->pwd],
-        ])->first();
+        // $u = User::where([
+        //     ['email', $req->em],
+        //     ['password', $req->pwd],
+        // ])->first();
 
-        if (isset($u)) {
-            session()->put('email', $u->email);
-            session()->put('name', $u->name);
-            return "<script>
-            alert('Welcome, " . session('name') . "');
-            location.href='/product';
-            </script>";
-        }
-
-        return redirect('/login')->with('msg', 'Email atau password salah!');
-
-        // if (Auth::attempt(['email'=>$req->em, 'password'=>$req->pwd])) {
-        //     return redirect('/product');
+        // if (isset($u)) {
+        //     session()->put('email', $u->email);
+        //     session()->put('name', $u->name);
+        //     return "<script>
+        //     alert('Welcome, " . session('name') . "');
+        //     location.href='/product';
+        //     </script>";
         // }
+
         // return redirect('/login')->with('msg', 'Email atau password salah!');
+
+        if (Auth::attempt(['email'=>$req->em, 'password'=>$req->pwd])) {
+            return redirect('/product');
+        }
+        return redirect('/login')->with('msg', 'Email atau password salah!');
     }
 }
